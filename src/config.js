0e1bdb9908394262f987eeba1805c973e99a240a
G.Linaris <gilfanov.ll@ozna.ru>
2023-07-22 08:30:19 +0500

    Change config

--- src/config.js
+++ src/config.js
@@ -1,18 +1,1 @@
-const _config = {
-    url: {
-        rest: window.location.protocol + '//' + window.location.hostname
-    },
-    port: {
-        rest: '9000',
-    },
-};
-
-const config = Object.freeze({
-    ..._config,
-    addr: {
-        rest: `${_config.url.rest}:${_config.port.rest}`,
-    },
-});
-
-export const REST_API_URL = config.addr.rest;
-export default config;
+export const REST_API_URL = 'https://beejee-server.onrender.com';
